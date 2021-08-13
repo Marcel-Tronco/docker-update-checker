2b7b7ce9700d4bacde8214320601d795a92202e7
Marcel <Marcel.Klotz@posteo.de>
2021-08-13 15:27:58 +0200

    Update_container func done

--- docker_updater.py
+++ docker_updater.py
@@ -3,7 +3,7 @@ import urllib.parse
 import datetime
 import dateutil.parser as parser
 import docker
-from typing import List
+from typing import List, Union, Tuple
 
 class DockerApiError(Exception):
   def __init__(self):
@@ -30,13 +30,11 @@ def clean_string(str: str) -> str:
   not_implemented_yet("clean_string")
   return str
 
-def get_image_tags(repo: str, image: str, page: int = 1) -> List[dict]:
-
-  url = f"https://hub.docker.com/v2/repositories/{clean_string(repo)}/{clean_string(image)}/tags?page_size=1&page={page}"
-  print(url)
+def get_image_tags(repo: str, image: str, page: int = 1) -> dict:
+  url = f"https://hub.docker.com/v2/repositories/{clean_string(repo)}/{clean_string(image)}/tags?page_size=1&page={clean_string(str(page))}"
   r = requests.get(url)
   json = r.json()
-  return json["results"]
+  return json
   # todo: - handling for multipage cases
 
 def load_running_containers() -> List[dict]:
@@ -47,7 +45,10 @@ def load_running_containers() -> List[dict]:
     "image_id": "sha256:2c9e87da2df22ee5b612e8c0fabd6c0961ef17c1a7c3ce4481115adf72940774",
     "tag": "4.1.0",
     "version_date": "2021-03-10T13:37:34.734372Z",
-    "open_update": None
+    "open_update": {
+      "tag_updates": None,
+      "new_tags": []
+      }
   }]
 
 def save_container_data(updated_containers: List[dict]) -> None:
@@ -55,23 +56,48 @@ def save_container_data(updated_containers: List[dict]) -> None:
   not_implemented_yet("save_container_data")
   return
 
-def check_image(container_data: List[dict], image_tags: List[dict]) -> None:
+def get_current_tag_data(current_tag: str, image_tags: dict) -> Union[dict, None]:
+  for tag_data in image_tags["results"]:
+    if current_tag == tag_data["name"]:
+      return tag_data["name"]
+  return None
+
+def get_tag_updates(container_data: dict, current_tag: dict) -> Tuple[Union[dict, None], bool]:
   # todo
-  not_implemented_yet("check_image")
-  return
+  not_implemented_yet("get_tag_updates")
+  return None, True
+
+def get_new_tags(container_data: dict, image_tags: dict) -> Tuple[Union[dict, None], bool]:
+  # todo
+  not_implemented_yet("get_new_tags")
+  return None, True
 
 def update_container(container: dict) -> dict:
-  # todo
-  not_implemented_yet("update_container")
-  try:
-    image_tags = get_image_tags(container["repo"], container["image_name"])
-    update_necessary = check_image(container, image_tags)
-    if update_necessary == None:
-      return container
-    else:
-      container["open_update"] = update_necessary()
-  except Exception:
-    raise DockerApiError #from None
+  current_tag_done = False
+  new_tags_done = False
+  new_tags = []
+  tag_updates = None
+
+  while True:
+    try:
+      image_tags = get_image_tags(container["repo"], container["image_name"])
+    except Exception:
+      raise DockerApiError ## from None
+    if not current_tag_done:
+      tag_updates, current_tag_done = get_tag_updates(container, image_tags)
+    if not new_tags_done:
+      tmp, new_tags_done = get_new_tags(container, image_tags)
+      if tmp:
+        new_tags += tmp
+    next_image_tag_url = image_tags["next"]
+    if (new_tags_done and current_tag_done) or next_image_tag_url == None:
+      break
+  container["open_update"] = {
+    "tag_updates": tag_updates,
+    "new_tags": new_tags
+  }
+  return container
+
 
 def gather_update_info(old_containers: List[dict], updated_containers: List[dict]) -> str:
   # todo
